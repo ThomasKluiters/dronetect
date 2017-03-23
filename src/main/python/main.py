7d40138be446d97ceb43af8b5a944f884ae1bc98
leonoverweel <l.overweel@gmail.com>
2017-03-23 12:10:14 +0100

    Process .wav + .avi files in video folder

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,24 +1,39 @@
-import os
 import sys
+from os import listdir, path
 
 def main(video_folder, database_location):
     """Process each video in the folder"""
     
     # Validate parameters
-    
-    if not os.path.isdir(video_folder):
+    if not path.isdir(video_folder):
         print 'Invalid video directory path: {}'.format(video_folder)
         return
         
-    if not os.path.exists(database_location):
+    if not path.exists(database_location):
         print 'Invalid database location: {}'.format(database_location)
         return
     
-    print 'Videos at: {}'.format(video_folder)
-    print 'Database at: {}'.format(database_location)
-
-if __name__ == "__main__":
+    # Get data
+    video_files = listdir(video_folder)
+    video_ids   = [f.split('.')[0] for f in video_files if f.endswith('.avi')]
     
+    for video_id in video_ids:
+        
+        # Make sure both a .avi and .wav file are available for this id
+        video_location = path.join(video_folder, '{}.avi'.format(video_id))
+        audio_location = path.join(video_folder, '{}.wav'.format(video_id))
+        
+        if not path.exists(video_location) or not path.exists(audio_location):
+            continue
+        
+        # Process video
+        print 'Processing `{}`...'.format(video_id) 
+        print video_location
+        print audio_location
+        print
+        
+if __name__ == "__main__":
+
     if len(sys.argv) == 3:
         main(sys.argv[1], sys.argv[2])
     else:
