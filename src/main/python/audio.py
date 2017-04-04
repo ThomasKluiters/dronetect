2fdbd330d1dd67b64863301e4ec9e8fd7e34a2b4
daniel-vos <daniel.vos.d@gmail.com>
2017-04-04 23:56:08 +0200

    Add function to audio that will enable drone classification

--- src/main/python/audio.py
+++ src/main/python/audio.py
@@ -3,14 +3,33 @@ import sys
 from scipy import signal
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import specgram
+import numpy as np
 
 def process(samples, f):    
-    print samples
-    specgram(samples, NFFT=256, Fs=f)
+    if not isinstance(samples[0], np.int16):
+        samples = samples[:,0]
+    specs, freqs, t, x = specgram(samples, NFFT=256, Fs=f)
+    specs /= np.sum(specs)
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [sec]')
     plt.show()
-
+    
+    summed = [sum(x) for x in specs]
+    plt.plot(freqs, summed)
+    plt.show()
+    
+    band = [abs(x-5680)<250 for x in freqs]
+    
+    result = []
+    for i in range(len(summed)):
+        if band[i]:
+            result.append(summed[i])
+        else:
+            result.append(0)
+            
+    plt.plot(freqs, result)
+    plt.show()
+    
 if __name__ == '__main__':
     if len(sys.argv) == 3:
         process(sys.argv[1], sys.argv[2])
