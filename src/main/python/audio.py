920cbc3e2c8c86437f5967bd0f3c7b0a5ab20532
daniel-vos <daniel.vos.d@gmail.com>
2017-04-04 11:42:03 +0200

    Remove commented code switch to mono audio

--- src/main/python/audio.py
+++ src/main/python/audio.py
@@ -3,15 +3,9 @@ from scipy import signal
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import specgram
 
-def process(samples, f):
-    #f, t, Sxx = signal.specgram(samples, f)
-    #plt.pcolormesh(t, f, Sxx)
-    #plt.ylabel('Frequency [Hz]')
-    #plt.xlabel('Time [sec]')
-    #plt.show()
-    
-    print samples[:,0]
-    specgram(samples[:,0], NFFT=256, Fs=f)
+def process(samples, f):    
+    print samples
+    specgram(samples, NFFT=256, Fs=f)
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [sec]')
     plt.show()
