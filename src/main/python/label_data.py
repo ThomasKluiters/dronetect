2f32a254316e754ee0ecd647ff914bfd3a65d6d3
leonoverweel <l.overweel@gmail.com>
2017-04-04 14:30:05 +0200

    Playback color videos
    Instead of black and white

--- src/main/python/label_data.py
+++ src/main/python/label_data.py
@@ -73,8 +73,7 @@ def main(video_folder, database_location):
             while(cap.isOpened()):
                 ret, frame = cap.read()
                 if ret == True:
-                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                    cv2.imshow('frame', gray)
+                    cv2.imshow('frame', frame)
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
                 else:
