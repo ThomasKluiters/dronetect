d0a4c8cd755865f84902abf61dd8e7d107ca2a49
daniel-vos <daniel.vos.d@gmail.com>
2017-04-05 11:46:46 +0200

    Add comments

--- src/main/python/audio.py
+++ src/main/python/audio.py
@@ -5,15 +5,19 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import specgram
 import numpy as np
 
-def process(samples, f):    
+def process(samples, f):
+    """Process an audio clip and decide if it contains drone-like sound"""
+    # Take only the first channel in stereo audio
     if not isinstance(samples[0], np.int16):
         samples = samples[:,0]
+        
+    # Create spectrogram and band-pass filter
     specs, freqs, t, x = specgram(samples, NFFT=256, Fs=f)
     specs /= np.sum(specs)
-    
     summed = [sum(x) for x in specs]
     band = [abs(x-5680)<250 for x in freqs]
     
+    # Apply band-pass filter
     result = []
     score = 0
     for i in range(len(summed)):
@@ -23,6 +27,7 @@ def process(samples, f):
         else:
             result.append(0)
     
+    # Classify as drone if more than 0.7% of the sound is in then 5680 Hz range
     drone = score > 0.007
     print drone
     return drone
